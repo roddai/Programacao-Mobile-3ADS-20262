803d87c13d1f5a53c5363d7034a3b98a0319e06e
Lucas Pereira de Souza <23011167@fecap.br>
2026-08-25 22:34:26 -0300

    ta completo

--- Aula04/Exemplo1/app/src/main/java/com/example/aula04/Livro.java
+++ Aula04/Exemplo1/app/src/main/java/com/example/aula04/Livro.java
@@ -1,4 +1,19 @@
 package com.example.aula04;
 
 public class Livro {
+private String titulo;
+private String autor;
+private int anoPublicacao;
+
+public Livro(String titulo,String autor,int anoPublicacao){
+
+    this.titulo = titulo;
+    this.titulo = autor;
+    this.anoPublicacao = anoPublicacao;
+
+}
+public String getInfo(){
+    return "Titulo" + titulo + "\nAutor: " + autor + "\nAno" + anoPublicacao;
+
+}
 }
--- Aula04/Exemplo1/app/src/main/java/com/example/aula04/MainActivity.java
+++ Aula04/Exemplo1/app/src/main/java/com/example/aula04/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aula04;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+Livro livro1, livro2, livro3;
+TextView textInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        livro1 = new Livro("Desenvolvendo seu Primeiro aplicativo Android","lucas", 2014);
+        livro2= new Livro("programando","caua", 2024);
+        livro3 = new Livro("o amor é belo","arthur", 2015);
+        textInfo = findViewById(R.id.textInfo);
+
+        Button button1 = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(v ->textInfo.setText(livro1.getInfo())) ;
+        button2.setOnClickListener(v ->textInfo.setText(livro2.getInfo())) ;
+        button3.setOnClickListener(v ->textInfo.setText(livro3.getInfo())) ;
+
+        }
     }
-}

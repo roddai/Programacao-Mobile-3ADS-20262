6f7e347f7f4c38c59737152222958ba5dbf19197
Katiely de Jesus Silva <23011355@fecap.br>
2026-08-25 21:56:31 -0300

    Finaliza o pimeiro projeto da aula 4

---
+++ Aula04/Exemplo01/app/src/main/java/com/example/projetoaula04_ex1/Livro.java
@@ -0,0 +1,16 @@
+package com.example.projetoaula04_ex1;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+    public String getInfo(){
+        return "Título: " + titulo + "\nAutor: " + autor + "\nAno de publicação: " + anoPublicacao;
+    }
+}

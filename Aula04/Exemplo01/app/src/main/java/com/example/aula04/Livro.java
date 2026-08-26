ad6b9b78a1df3de2b3bfbfd694716123317c29a2
Maria Eduarda Barberino Olo <25028088@fecap.br>
2026-08-25 21:52:28 -0300

    Atualizado

--- Aula04/Exemplo01/app/src/main/java/com/example/aula04/Livro.java
+++ Aula04/Exemplo01/app/src/main/java/com/example/aula04/Livro.java
@@ -1,4 +1,17 @@
 package com.example.aula04;
 
 public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getInfo(){
+        return "Título" + titulo + "\nAutor" + autor + "\nAno" + anoPublicacao;
+    }
 }

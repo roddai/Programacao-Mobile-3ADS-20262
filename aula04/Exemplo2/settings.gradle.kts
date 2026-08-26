14a3339d45290046b1df1d9272ee8c0d0178db6f
Davi Bigotto Lourenço <23011119@fecap.br>
2026-08-25 22:32:02 -0300

    commit aula04 exemplo 2

---
+++ aula04/Exemplo2/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "aula04 - ex 2"
+include(":app")

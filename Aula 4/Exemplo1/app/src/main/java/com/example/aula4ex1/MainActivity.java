9eb4b03edc18a4102edf4143e0e345bac63b217a
Julia Gomes Basilio <25028157@fecap.br>
2026-08-25 21:20:39 -0300

    atualização

--- Aula 4/Exemplo1/app/src/main/java/com/example/aula4ex1/MainActivity.java
+++ Aula 4/Exemplo1/app/src/main/java/com/example/aula4ex1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aula4ex1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Livro livro1, livro2, livro3;
+
+    TextView textInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        livro1 = new Livro("primeiro Livro", "Primeiro Autor", 2024);
+        livro2 = new Livro("Segundo Livro", "Segundo Autor", 2020);
+        livro3 = new Livro("Terceiro Livro", "Terceiro Autor", 2021);
+
+        textInfo = findViewById(R.id.textInfo);
+
+        Button button1 = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(v -> textInfo.setText(livro1.getInfo()));
+        button2.setOnClickListener(v -> textInfo.setText(livro1.getInfo()));
+        button3.setOnClickListener(v -> textInfo.setText(livro1.getInfo()));
+
     }
 }

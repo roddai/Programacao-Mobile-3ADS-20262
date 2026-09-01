d8943fbedf75889ad5f57eae95ccbbd496e089a3
Leonardo Cruz Lamari <25028194@fecap.br>
2026-09-01 20:18:44 -0300

    Modifiquei o Main, e mexi na classe da Main

--- Aula5/app/src/main/java/com/example/projetoaula5/MainActivity.java
+++ Aula5/app/src/main/java/com/example/projetoaula5/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.projetoaula5;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editPeso, editAltura;
+    Button btnCalc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editPeso = findViewById(R.id.editPeso);
+        editAltura = findViewById(R.id.editAltura);
+        btnCalc = findViewById(R.id.btnCalc);
     }
 }

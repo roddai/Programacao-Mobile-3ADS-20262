54ca03dd2f7c0cb40fdb91fe36aa3455823d8a55
Rodrigo da Rosa <27532544869@fecap.br>
2026-09-01 21:42:53 -0300

    conclui primeira parte

--- Aula05/app/src/main/java/com/example/projetoaula05/MainActivity.java
+++ Aula05/app/src/main/java/com/example/projetoaula05/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.projetoaula05;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editPeso, editAltura;
+    Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,35 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        editPeso = findViewById(R.id.editPeso);
+        editAltura = findViewById(R.id.editAltura);
+        btnCalcular = findViewById(R.id.btnCalcular);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double peso = Double.parseDouble(editPeso.getText().toString());
+                double altura = Double.parseDouble(editAltura.getText().toString());
+                double imc = peso / (altura * altura);
+
+                Intent intent;
+
+                if (imc < 18.5) {
+                    intent = new Intent(MainActivity.this,ResultadoAbaixoDoPesoActivity.class);
+                } else {
+                    intent = new Intent(MainActivity.this, ResultadoObesidadeClasse2Activity.class);
+                }
+
+                startActivity(intent);
+
+                /*Abaixo do 18,5:
+                Abaixo do peso18,5 a 24,9:
+                Peso normal ou adequado25,0 a 29,9:
+                Sobrepeso30,0 a 39,9: Obesidade
+                Acima de 40,0 Obesidade grave*/
+            }
         });
     }
 }
--- Aula05/app/src/main/java/com/example/projetoaula05/ResultadoAbaixoDoPesoActivity.java
+++ Aula05/app/src/main/java/com/example/projetoaula05/ResultadoAbaixoDoPesoActivity.java
@@ -1,4 +1,20 @@
 package com.example.projetoaula05;
 
-public class ResultadoAbaixoDoPesoActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultadoAbaixoDoPesoActivity extends AppCompatActivity {
+    Button btnVoltar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resultado_abaixo_peso);
+
+
+    }
 }

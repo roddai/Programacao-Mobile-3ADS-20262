37f67d19ff8ed08bd9d3c9e02b935d1ea41dc810
Leonardo Cruz Lamari <25028194@fecap.br>
2026-09-01 21:50:39 -0300

    Adicionei os outros manifestos

--- Aula5/app/src/main/java/com/example/projetoaula5/ResultadoAbaixoDoPesoActivity.java
+++ Aula5/app/src/main/java/com/example/projetoaula5/ResultadoAbaixoDoPesoActivity.java
@@ -1,4 +1,26 @@
 package com.example.projetoaula5;
 
-public class ResultadoAbaixoDoPesoActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultadoAbaixoDoPesoActivity extends AppCompatActivity {
+
+    Button btnVoltar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resultado_abaixo_peso);
+
+    }
 }

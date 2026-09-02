5622d32a894da8d6ccbe9e7625c6f87e060cece5
Fátima Eduarda Gomes Balbino <25028242@fecap.br>
2026-09-01 22:20:54 -0300

    Atualização

--- Aula05/app/src/main/java/com/example/projetoaula05/ResultadoAbaixoDoPesoActivity.java
+++ Aula05/app/src/main/java/com/example/projetoaula05/ResultadoAbaixoDoPesoActivity.java
@@ -1,4 +1,25 @@
 package com.example.projetoaula05;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class ResultadoAbaixoDoPesoActivity {
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultadoAbaixoDoPesoActivity extends AppCompatActivity {
+
+    Button btnCalcular;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resultado_abaixo_do_peso);
+
+    }
 }

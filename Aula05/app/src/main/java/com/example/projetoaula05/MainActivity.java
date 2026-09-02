c7b800f87900db10198970a907f24901b316f401
Rodrigo da Rosa <27532544869@fecap.br>
2026-09-01 21:50:10 -0300

    ajusta detalhes dem MainActivity

--- Aula05/app/src/main/java/com/example/projetoaula05/MainActivity.java
+++ Aula05/app/src/main/java/com/example/projetoaula05/MainActivity.java
@@ -5,12 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,13 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         editPeso = findViewById(R.id.editPeso);
         editAltura = findViewById(R.id.editAltura);
@@ -37,24 +26,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 double peso = Double.parseDouble(editPeso.getText().toString());
                 double altura = Double.parseDouble(editAltura.getText().toString());
-                double imc = peso / (altura * altura);
+                double imc = peso / (altura + altura);
 
                 Intent intent;
 
                 if (imc < 18.5) {
-                    intent = new Intent(MainActivity.this,ResultadoAbaixoDoPesoActivity.class);
+                    intent = new Intent(MainActivity.this, ResultadoAbaixoDoPesoActivity.class);
+                } else if (imc < 25) {
+                    intent = new Intent(MainActivity.this, ResultadoNormalActivity.class);
+                } else if (imc < 30) {
+                    intent = new Intent(MainActivity.this, ResultadoSobrepesoActivity.class);
+                } else if (imc < 35) {
+                    intent = new Intent(MainActivity.this, ResultadoObesidadeClasse1Activity.class);
                 } else {
                     intent = new Intent(MainActivity.this, ResultadoObesidadeClasse2Activity.class);
                 }
-
                 startActivity(intent);
+            }
 
-                /*Abaixo do 18,5:
+            /*Abaixo do 18,5:
                 Abaixo do peso18,5 a 24,9:
                 Peso normal ou adequado25,0 a 29,9:
                 Sobrepeso30,0 a 39,9: Obesidade
                 Acima de 40,0 Obesidade grave*/
-            }
         });
     }
 }
+

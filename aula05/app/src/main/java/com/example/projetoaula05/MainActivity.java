dc65ab171420fb19fbdd33fc97c37227b43f8ab9
Henrique Jorge Martins Figueiredo <24026809@fecap.br>
2026-09-01 22:31:05 -0300

    Calculador IMC

--- aula05/app/src/main/java/com/example/projetoaula05/MainActivity.java
+++ aula05/app/src/main/java/com/example/projetoaula05/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.projetoaula05;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editPeso, editAltura;
+    Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,36 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        editPeso = findViewById(R.id.editPeso);
+        editAltura = findViewById(R.id.editAltura);
+        btnCalcular = findViewById(R.id.btnCalcular);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double peso = Double.parseDouble(editPeso.getText().toString());
+                double altura = Double.parseDouble(editAltura.getText().toString());
+                double imc = peso / (altura * altura);
+
+                Intent intent;
+
+                if (imc < 18.5) {
+                    intent = new Intent(MainActivity.this, ResultadoAbaixoDoPeso.class);
+                } else if (imc < 25) {
+                    intent = new Intent(MainActivity.this, ResultadoNormalActivity.class);
+                } else if (imc < 30){
+                    intent = new Intent(MainActivity.this, ResultadoSobrePesoActivity.class);
+                } else if (imc < 35) {
+                    intent = new Intent(MainActivity.this, ResultadoObesidadeClasse1Activity.class);
+                } else {
+                    intent = new Intent(MainActivity.this, ResultadoObesidadeClasse2Activity.class);
+                }
+                startActivity(intent);
+            }
+
+
         });
     }
 }
--- aula05/app/src/main/java/com/example/projetoaula05/ResultadoObesidadeClasse2Activity.java
+++ aula05/app/src/main/java/com/example/projetoaula05/ResultadoObesidadeClasse2Activity.java
@@ -1,4 +1,22 @@
 package com.example.projetoaula05;
 
-public class ResultadoObesidadeClasse2Activity {
-}
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultadoObesidadeClasse2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activyty_resultado_obesidade_2);
+    }
+};

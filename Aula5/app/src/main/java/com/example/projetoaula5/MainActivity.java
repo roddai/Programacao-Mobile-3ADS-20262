393db44ee83e9dad2d144814764f78a412ad5953
Leonardo Cruz Lamari <25028194@fecap.br>
2026-09-01 21:35:52 -0300

    Terminei a activity da main e estou tentanto ir para a tela de abaixo do peso

--- Aula5/app/src/main/java/com/example/projetoaula5/MainActivity.java
+++ Aula5/app/src/main/java/com/example/projetoaula5/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.projetoaula5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -29,5 +31,30 @@ public class MainActivity extends AppCompatActivity {
         editPeso = findViewById(R.id.editPeso);
         editAltura = findViewById(R.id.editAltura);
         btnCalc = findViewById(R.id.btnCalc);
+
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double peso = Double.parseDouble(editPeso.getText().toString());
+                double altura = Double.parseDouble(editAltura.getText().toString());
+                double imc = peso/(altura*altura);
+
+                Intent intent;
+
+                if (imc < 18.5){
+                    intent = new Intent(MainActivity.this, ResultadoAbaixoDoPesoActivity.class);
+                }else if (imc >= 18.5 && imc <= 24.9 ){
+                    intent = new Intent(MainActivity.this, ResultadoNormalActivity.class);
+                }else if (imc >= 25 && imc <= 29.9 ){
+                    intent = new Intent(MainActivity.this, ResultadoSobrepesoActivity.class);
+                }else if (imc >= 30 && imc <= 34.9 ){
+                    intent = new Intent(MainActivity.this, ResultadoObesidadeClasse1Activity.class);
+                }else {
+                    intent = new Intent(MainActivity.this, ResultadoObesidadeClasse2Activity.class);
+                }
+
+                startActivity(intent);
+            }
+        });
     }
 }

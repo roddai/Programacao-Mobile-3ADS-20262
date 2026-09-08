66d80e14b0585b15d356ca25785e5f404f0a5baf
Lucas Pereira de Souza <23011167@fecap.br>
2026-09-08 19:39:24 -0300

    criação da classe Pet

---
+++ Aula06-Pets/app/src/main/java/com/example/app_pets/pet.java
@@ -0,0 +1,19 @@
+package com.example.app_pets;
+
+import java.util.Date;
+
+public class pet {
+    private String cor;
+    private String raca;
+    private int idade;
+    private Date aniversario;
+
+    public pet(String cor, String raca, int idade, Date aniversario) {
+
+        this.cor = cor;
+        this.raca = raca;
+        this.idade = idade;
+        this.aniversario = aniversario;
+
+    }
+}

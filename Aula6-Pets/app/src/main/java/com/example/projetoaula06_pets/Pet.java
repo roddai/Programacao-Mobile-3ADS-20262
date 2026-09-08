77d8981e87302c330d5f42984380e759e52cce2d
Leonardo Cruz Lamari <25028194@fecap.br>
2026-09-08 19:54:30 -0300

    Criei a classe Pet e o construtor

---
+++ Aula6-Pets/app/src/main/java/com/example/projetoaula06_pets/Pet.java
@@ -0,0 +1,20 @@
+package com.example.projetoaula06_pets;
+
+public class Pet {
+    public String nome;
+    public String cor;
+    public String raca;
+    public int idade;
+    public int niver;
+
+    public Pet(String nome,String cor,String raca, int idade, int niver)
+    {
+        this.nome = nome;
+        this.cor = cor;
+        this.raca = raca;
+        this.idade = idade;
+        this.niver = niver;
+    }
+}
+
+

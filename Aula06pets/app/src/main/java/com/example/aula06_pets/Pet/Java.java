60e8900a8c1a3eee1bbfc14998fef8896c5c608a
José Antonio da Silva de Aguiar <25028150@fecap.br>
2026-09-08 20:00:31 -0300

    criação da classe Pet V2

--- Aula06pets/app/src/main/java/com/example/aula06_pets/Pet/Java.java
+++ Aula06pets/app/src/main/java/com/example/aula06_pets/Pet/Java.java
@@ -1,4 +1,25 @@
 package com.example.aula06_pets.Pet;
 
+import android.content.Intent;
+
+import java.util.Date;
+
 public class Java {
+
+    public String nome;
+
+    public String cor;
+
+    public String raca;
+
+    public int idade;
+
+    public Date aniversario;
+
+    public Java (String nome, String cor, String raca, int idade, Date aniversario)
+        this.nome = nome;
+        this.cor = cor;
+        this.raca = raca;
+        this.idade = idade;
+        this.aniversario = aniversario;
 }

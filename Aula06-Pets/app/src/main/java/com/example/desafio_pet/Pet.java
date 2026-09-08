9e415a3add4d7526b9cc8fc20247df61135c03ae
Matheus Marques Quio <23011107@fecap.br>
2026-09-08 19:50:10 -0300

    criação da classe Pet

---
+++ Aula06-Pets/app/src/main/java/com/example/desafio_pet/Pet.java
@@ -0,0 +1,18 @@
+package com.example.desafio_pet;
+
+public class Pet {
+    private String nome;
+    private String raca;
+    private String cor;
+    private int idade;
+    private int data_de_aniversario;
+
+    public Pet(String nome, String raca, String cor, int idade, int data_de_aniversario){
+        this.nome = nome;
+        this.raca = raca;
+        this.cor = cor;
+        this.idade = idade;
+        this.data_de_aniversario = data_de_aniversario;
+    }
+
+}

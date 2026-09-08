1ba657e6419b35aaccdb163fb58da376688c71cb
Julia Gomes Basilio <25028157@fecap.br>
2026-09-08 19:49:10 -0300

    criação da classe Pet

---
+++ Aula 6 Pets/app/src/main/java/com/example/aula6pets/Pet.java
@@ -0,0 +1,17 @@
+package com.example.aula6pets;
+
+public class Pet {
+    public String nome;
+    public String cor;
+    public String raca;
+
+    public int idade;
+    public String aniversario;
+
+    public Pet (String nome, String cor, String raca, int idade, String aniversario)
+        this.nome = nome;
+        this.cor = cor;
+        this.raca = raca;
+        this.idade = idade;
+        this.aniversario = aniversario;
+}

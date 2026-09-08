e3a8594babddbbbb039e0d9d2b10e49fbbb44bd8
Arthur Carvalho Chagas <25028070@fecap.br>
2026-09-08 20:11:25 -0300

    criação da cÃlasse Pet

---
+++ Projetoaula06/app/src/main/java/com/example/projetoaula06/Pet.java
@@ -0,0 +1,25 @@
+package com.example.projetoaula06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Pet {
+    private String nome;
+
+    private String cor;
+
+    private int idade;
+
+    private String raca;
+
+    private String aniversario;
+
+    public Pet(String nome, String cor, int idade, String raca, String aniversario){
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+        this.raca = raca;
+        this.aniversario = aniversario;
+
+    }
+
+}

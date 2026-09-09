a98ae286558c234f30a990906a9df0d63985ae1b
Luccas Covre Araujo <25028294@fecap.br>
2026-09-08 22:16:03 -0300

    Adição do texto nos detalhes e mudança visual

--- Aula06-Pets/app/src/main/java/com/example/aula_06/MainActivity.java
+++ Aula06-Pets/app/src/main/java/com/example/aula_06/MainActivity.java
@@ -14,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     Button btn1, btn2, btn3, btn4, btn5, btn6;
 
+    Pet pet1, pet2, pet3, pet4, pet5, pet6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +34,83 @@ public class MainActivity extends AppCompatActivity {
         btn5 = findViewById(R.id.btn5);
         btn6 = findViewById(R.id.btn6);
 
+        pet1 = new Pet("Carlos", "Dourado", "Golden Retriever", 3, "02/02");
+        pet2 = new Pet("Ronaldo", "Malhado Marrom", "Border Collie", 2, "03/03");
+        pet3 = new Pet("Alvaro", "Preto", "Salsicha", 1, "04/04");
+        pet4 = new Pet("Lucas", "Rajado Cinza", "Rajado", 3, "02/02");
+        pet5 = new Pet("Bob the wizard", "Tuxedo", "Frajola", 1, "03/03");
+        pet6 = new Pet("Carlos2", "Rajado Beje", "Rajado", 5, "05/05");
+
         btn1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent;
 
-                intent = new Intent(MainActivity.this, MainDetalhes.class);
-                startActivity(intent);
+                Intent infoPet1= new Intent(MainActivity.this, MainDetalhes.class);
+                infoPet1.putExtra("pet", pet1.getInfo());
+
+                startActivity(infoPet1);
+            }
+
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent;
+
+                Intent infoPet2= new Intent(MainActivity.this, MainDetalhes.class);
+                infoPet2.putExtra("pet", pet2.getInfo());
+
+                startActivity(infoPet2);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent;
+
+                Intent infoPet3= new Intent(MainActivity.this, MainDetalhes.class);
+                infoPet3.putExtra("pet", pet3.getInfo());
+
+                startActivity(infoPet3);
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent;
+
+                Intent infoPet4= new Intent(MainActivity.this, MainDetalhes.class);
+                infoPet4.putExtra("pet", pet4.getInfo());
+
+                startActivity(infoPet4);
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent;
+
+                Intent infoPet5= new Intent(MainActivity.this, MainDetalhes.class);
+                infoPet5.putExtra("pet", pet5.getInfo());
+
+                startActivity(infoPet5);
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent;
+
+                Intent infoPet6= new Intent(MainActivity.this, MainDetalhes.class);
+                infoPet6.putExtra("pet", pet6.getInfo());
+
+                startActivity(infoPet6);
             }
         });
     }
--- Aula06-Pets/app/src/main/java/com/example/aula_06/MainDetalhes.java
+++ Aula06-Pets/app/src/main/java/com/example/aula_06/MainDetalhes.java
@@ -3,6 +3,7 @@ package com.example.aula_06;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainDetalhes extends AppCompatActivity {
+import java.text.BreakIterator;
 
+public class MainDetalhes extends AppCompatActivity {
+    TextView textInfo;
     Button btnBack;
 
         @Override
@@ -19,6 +22,13 @@ public class MainDetalhes extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.detalhes);
             btnBack = findViewById(R.id.btnBack);
+
+            textInfo = findViewById(R.id.textInfo);
+
+            String informacao = getIntent().getStringExtra("pet");
+
+            textInfo.setText( informacao );
+
             btnBack.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v){

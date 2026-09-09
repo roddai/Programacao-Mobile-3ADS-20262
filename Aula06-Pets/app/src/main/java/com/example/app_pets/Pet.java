fa500136ebee8e477d8daebe74d03a7ecea091af
Lucas Pereira de Souza <23011167@fecap.br>
2026-09-08 21:35:29 -0300

    Criação do layout da tela detalhes

--- Aula06-Pets/app/src/main/java/com/example/app_pets/Pet.java
+++ Aula06-Pets/app/src/main/java/com/example/app_pets/Pet.java
@@ -3,17 +3,21 @@ package com.example.app_pets;
 import java.util.Date;
 
 public class Pet {
+    private String nome;
     private String cor;
     private String raca;
     private int idade;
-    private Date aniversario;
+    private String aniversario;
 
-    public Pet(String cor, String raca, int idade, Date aniversario) {
-
+    public Pet(String nome,String cor, String raca, int idade, String aniversario) {
+        this.nome= nome;
         this.cor = cor;
         this.raca = raca;
         this.idade = idade;
         this.aniversario = aniversario;
 
+    }
+    public String getInfo() {
+        return "Nome: " + nome +"\nCor: " + cor + "\nRaça: " + raca + "\nIdade: " + idade + "\nAniversário: " + aniversario;
     }
 }

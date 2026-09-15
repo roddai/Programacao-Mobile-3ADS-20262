2752880b4e77c6f2f83588d66d7219c1127957cb
Álvaro Jordão de Carvalho Palazzin <25028302@fecap.br>
2026-09-15 20:05:05 -0300

    declarações

--- Aula07/app/src/main/java/com/example/aula07/MainActivity.java
+++ Aula07/app/src/main/java/com/example/aula07/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aula07;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultado;
+
+    ImageView imageMaquina, imagePedra, imagePapel, imageTesoura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        resultado = findViewById(R.id.resultado);
+
+        imageMaquina = findViewById(R.id.imageMaquina);
+        imagePedra = findViewById(R.id.imagePedra);
+        imagePapel = findViewById(R.id.imagePapel);
+        imageTesoura = findViewById(R.id.imageTesoura);
+
+
+
     }
 }

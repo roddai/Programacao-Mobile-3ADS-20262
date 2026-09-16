39a511faa9b798795781a5409286d4a6eb91a3de
Davi Bigotto Lourenço <23011119@fecap.br>
2026-09-15 22:09:06 -0300

    Finaliza parte 2

--- aula07jokempo/app/src/main/java/com/example/aula07_jokempo/MainActivity.java
+++ aula07jokempo/app/src/main/java/com/example/aula07_jokempo/MainActivity.java
@@ -14,12 +14,18 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-
     TextView txt_resultado;
     ImageView imgMaquina;
     ImageView imgPedra;
     ImageView imgPapel;
     ImageView imgTesoura;
+
+    ImageView imgSelecaoFem;
+    ImageView imgSelecaoMasc;
+    ImageView imgEscolhaMaquina;
+    ImageView imgPersonagemEscolhido;
+    ImageView imgEscolhaUsuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,46 +43,66 @@ public class MainActivity extends AppCompatActivity {
         imgTesoura = findViewById(R.id.img_tesoura);
         imgPedra = findViewById(R.id.img_pedra);
 
+        imgSelecaoFem = findViewById(R.id.img_selecao_fem);
+        imgSelecaoMasc = findViewById(R.id.img_selecao_masc);
+        imgEscolhaMaquina = findViewById(R.id.img_escolha_maquina);
+        imgPersonagemEscolhido = findViewById(R.id.img_personagem_escolhido);
+        imgEscolhaUsuario = findViewById(R.id.img_escolha_usuario);
+
+        imgSelecaoFem.setOnClickListener(v -> selecionarPersonagem("feminino"));
+        imgSelecaoMasc.setOnClickListener(v -> selecionarPersonagem("masculino"));
+
         imgPedra.setOnClickListener(v -> jogar("pedra"));
         imgPapel.setOnClickListener(v -> jogar("papel"));
         imgTesoura.setOnClickListener(v -> jogar("tesoura"));
+    }
 
+    public void selecionarPersonagem(String sexo) {
+        if(sexo.equals("feminino")) {
+            imgPersonagemEscolhido.setImageResource(R.drawable.usuario_fem);
+        } else {
+            imgPersonagemEscolhido.setImageResource(R.drawable.usuario_masc);
+        }
     }
 
     public void jogar(String escolhaUsuario){
         String[] opcoes = {"pedra", "papel", "tesoura"};
 
-        int numero = new Random().nextInt( 3);
+        int numero = new Random().nextInt(3);
         String escolhaMaquina = opcoes[numero];
-
 
         switch(escolhaMaquina) {
             case "pedra":
-                imgMaquina.setImageResource(R.drawable.pedra);
+                imgEscolhaMaquina.setImageResource(R.drawable.pedra);
                 break;
-
             case "papel":
-                imgMaquina.setImageResource(R.drawable.papel);
+                imgEscolhaMaquina.setImageResource(R.drawable.papel);
                 break;
-
             case "tesoura":
-                imgMaquina.setImageResource(R.drawable.tesoura);
+                imgEscolhaMaquina.setImageResource(R.drawable.tesoura);
                 break;
+        }
 
+        switch(escolhaUsuario) {
+            case "pedra":
+                imgEscolhaUsuario.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imgEscolhaUsuario.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imgEscolhaUsuario.setImageResource(R.drawable.tesoura);
+                break;
         }
 
         if(escolhaUsuario.equals(escolhaMaquina)){
             txt_resultado.setText("Empate!!!");
-
         } else if((escolhaUsuario.equals("pedra") && escolhaMaquina.equals("tesoura")) ||
-                (escolhaUsuario.equals("papel") && escolhaMaquina.equals("pedra") ||
-                (escolhaUsuario.equals("tesoura") && escolhaMaquina.equals("papel")))) {
+                (escolhaUsuario.equals("papel") && escolhaMaquina.equals("pedra")) ||
+                (escolhaUsuario.equals("tesoura") && escolhaMaquina.equals("papel"))) {
             txt_resultado.setText("Você ganhou!!!");
         } else {
             txt_resultado.setText("Você perdeu!!!");
-
-
-        }
-
         }
     }
+}

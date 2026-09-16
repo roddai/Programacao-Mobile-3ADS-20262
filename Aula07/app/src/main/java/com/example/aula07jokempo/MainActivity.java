950b76d8d323e38c2a72df507f02c82428a7a255
Maria Eduarda Barberino Olo <25028088@fecap.br>
2026-09-15 22:36:53 -0300

    Parte 2 aula 07

--- Aula07/app/src/main/java/com/example/aula07jokempo/MainActivity.java
+++ Aula07/app/src/main/java/com/example/aula07jokempo/MainActivity.java
@@ -14,63 +14,71 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textResultado;
-    ImageView imgMaquina, imgPapel, imgPedra, imgTesoura;
+        TextView textResultado, textPersonagem;
+        ImageView imgMaquina, imgPedra, imgPapel, imgTesoura, imgMasc, imgFem, imgFemi, imgInterrogacao, imgInterrogacao2;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        @Overridegit
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_main);
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
 
-        textResultado = findViewById(R.id.txtResultado);
-        imgMaquina = findViewById(R.id.imgMaquina);
-        imgPedra = findViewById(R.id.imgPedra);
-        imgPapel = findViewById(R.id.imgPapel);
-        imgTesoura = findViewById(R.id.imgTesoura);
+            textResultado = findViewById(R.id.textResultado);
 
-        imgTesoura.setOnClickListener(v -> jogar("tesoura"));
-        imgPapel.setOnClickListener(v -> jogar("papel"));
-        imgPedra.setOnClickListener(v -> jogar("pedra"));
-    }
+            imgMaquina = findViewById(R.id.imgMaquina);
+            imgPedra = findViewById(R.id.imgPedra);
+            imgPapel = findViewById(R.id.imgPapel);
+            imgTesoura = findViewById(R.id.imgTesoura);
+
+            textPersonagem = findViewById(R.id.textPersonagem);
+
+            imgFem = findViewById(R.id.imgFem);
+            imgFemi = findViewById(R.id.imgFemi);
+            imgMasc = findViewById(R.id.imgMasc);
 
 
-    public void jogar (String escolhadDousuario) {
-        String[] opcoes = {"pedra", "papel", "tesoura"};
 
-        int numero = new Random().nextInt(3);
+            imgPedra.setOnClickListener(v -> jogar("pedra"));
+            imgPapel.setOnClickListener(v -> jogar("papel"));
+            imgTesoura.setOnClickListener(v -> jogar("tesoura"));
 
-        String escolhaDaMquina = opcoes [numero]; // ele vai ver em opções o que tem lá e ver o numero que tem no random. 0,1 ou 2.
-
-        switch (escolhaDaMquina) {
-            case "pedra":
-                imgMaquina.setImageResource(R.drawable.pedra);
-                break;
-
-            case "papel":
-                imgMaquina.setImageResource(R.drawable.papel);
-                break;
-
-            case "tesoura":
-                imgMaquina.setImageResource(R.drawable.tesoura);
-                break;
-
+            imgMasc.setOnClickListener(v-> imgFemi.setImageResource(R.drawable.usuario_masc));
+            imgFem.setOnClickListener(v-> imgFemi.setImageResource(R.drawable.usuario_fem));
         }
 
-        if (escolhadDousuario.equals(escolhaDaMquina)) {
-            textResultado.setText("Empate!");
-        } else if ((escolhadDousuario.equals("pedra") && escolhaDaMquina.equals("tesoura")) ||
-                (escolhadDousuario.equals("papel") && escolhaDaMquina.equals("pedra")) ||
-                ((escolhadDousuario.equals("tesoura") && escolhaDaMquina.equals("papel")))) {
-            textResultado.setText("Você Ganhou");
-        } else {
-            textResultado.setText("Você Perdeu");
+        public void jogar(String escolhaDoUsuario){
+            String[] opcoes = {"pedra", "papel","tesoura"};
 
+
+            int numero = new Random().nextInt(3);
+            String escolhadaMaquina = opcoes[numero];
+
+            switch (escolhadaMaquina) {
+                case "pedra" :
+                    imgMaquina.setImageResource(R.drawable.pedra);
+                    break;
+                case "papel" :
+                    imgMaquina.setImageResource(R.drawable.papel);
+                    break;
+                case "tesoura" :
+                    imgMaquina.setImageResource(R.drawable.tesoura);
+                    break;
+            }
+
+            if (escolhaDoUsuario.equals(escolhadaMaquina)) {
+                textResultado.setText("Empate");
+            }else if ((escolhaDoUsuario.equals("pedra") && escolhadaMaquina.equals("tesoura")) ||
+                    (escolhaDoUsuario.equals("papel") && escolhadaMaquina.equals("pedra")) ||
+                    (escolhaDoUsuario.equals("tesoura") && escolhadaMaquina.equals("papel"))) {
+                textResultado.setText("Você ganhou!");
+            }else {
+                textResultado.setText("Você perdeu!");
+            }
         }
     }
-}
+

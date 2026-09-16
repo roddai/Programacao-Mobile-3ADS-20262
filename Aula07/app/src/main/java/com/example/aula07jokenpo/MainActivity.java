e71d7586433a5e0749c81c84ed48e5bb3cbaa29d
Fátima Eduarda Gomes Balbino <25028242@fecap.br>
2026-09-15 22:36:27 -0300

    Aula07 parte 2

--- Aula07/app/src/main/java/com/example/aula07jokenpo/MainActivity.java
+++ Aula07/app/src/main/java/com/example/aula07jokenpo/MainActivity.java
@@ -16,8 +16,8 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textResultado;
-    ImageView imgMaquina, imgPapel, imgPedra, imgTesoura;
+    TextView textResultado, txtEscolhaSimbolo, txtEscolhaPersonagem;
+    ImageView imgMaquina, imgPapel, imgPedra, imgTesoura, imgInterrogacao, img_usuario_fem, imgInterrogacao2, img_usuario_fem2, img_usuario_masc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +31,25 @@ public class MainActivity extends AppCompatActivity {
         });
 
         textResultado = findViewById(R.id.txtResultado);   // AQUI PEGA O OUTRO TEXT QUE CIE NO ACTIVITY
+        txtEscolhaSimbolo = findViewById(R.id.txtEscolhaSimbolo);
+        txtEscolhaPersonagem = findViewById(R.id.txtEscolhaPersonagem);
+
         imgMaquina = findViewById(R.id.imgMaquina); // ou seja estamos atrelando, depois de criar as variaveis la em cima
         imgPedra = findViewById(R.id.imgPedra); // vamos agora atrelar as variaveis com os elementos que temos no activity_main
         imgPapel = findViewById(R.id.imgPapel);
         imgTesoura = findViewById(R.id.imgTesoura);
+        imgInterrogacao = findViewById(R.id.imgInterrogacao);
+        img_usuario_fem = findViewById(R.id.img_usuario_fem);
+        imgInterrogacao2 = findViewById(R.id.imgInterrogacao2);
+        img_usuario_fem2 = findViewById(R.id.img_usuario_fem2);
+        img_usuario_masc = findViewById(R.id.img_usuario_masc);
 
         imgTesoura.setOnClickListener(v -> jogar("tesoura"));
         imgPapel.setOnClickListener(v -> jogar("papel"));
         imgPedra.setOnClickListener(v -> jogar("pedra"));
+
+        img_usuario_fem2.setOnClickListener(v -> img_usuario_fem.setImageResource(R.drawable.usuario_fem));
+        img_usuario_masc.setOnClickListener(v -> img_usuario_fem.setImageResource(R.drawable.usuario_masc));
     }
 
     // DEPOIS DE ATRELAR PRECISAMOS QUE O CODIGO TENHA QUAL A ESCOLHA DA MAQUINA E QUAL A NOSSA ESCOLHA
@@ -52,9 +63,9 @@ public class MainActivity extends AppCompatActivity {
         // int numero = new Random().nextInt(3) é o que vai criar a escolha randomica.
         // nextInt(3) o 3 significa que ele vai escolher entre os 3 elementos entro do array/vetor. ou seja, 3 pulos.
 
-        String escolhaDaMquina = opcoes [numero]; // ele vai ver em opções o que tem lá e ver o numero que tem no random. 0,1 ou 2.
+        String escolhaDaMaquina = opcoes [numero]; // ele vai ver em opções o que tem lá e ver o numero que tem no random. 0,1 ou 2.
 
-        switch (escolhaDaMquina) {
+        switch (escolhaDaMaquina) {
             case "pedra":
                 imgMaquina.setImageResource(R.drawable.pedra); // o Set é para mudar a imagem
                 break; // sempre que usamos um switch case devemos colocar o break
@@ -66,14 +77,24 @@ public class MainActivity extends AppCompatActivity {
             case "tesoura":
                 imgMaquina.setImageResource(R.drawable.tesoura); // o Set é para mudar a imagem
                 break; // sempre que usamos um switch case devemos colocar o break
-
+        }
+        switch (escolhadDousuario) {
+            case "pedra":
+                imgInterrogacao2.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imgInterrogacao2.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imgInterrogacao2.setImageResource(R.drawable.tesoura);
+                break;
         }
 
-        if (escolhadDousuario.equals(escolhaDaMquina)) {
+        if (escolhadDousuario.equals(escolhaDaMaquina)) {
             textResultado.setText("Empate!");
-        } else if ((escolhadDousuario.equals("pedra") && escolhaDaMquina.equals("tesoura")) ||
-                (escolhadDousuario.equals("papel") && escolhaDaMquina.equals("pedra")) ||
-                ((escolhadDousuario.equals("tesoura") && escolhaDaMquina.equals("papel")))) {
+        } else if ((escolhadDousuario.equals("pedra") && escolhaDaMaquina.equals("tesoura")) ||
+                (escolhadDousuario.equals("papel") && escolhaDaMaquina.equals("pedra")) ||
+                ((escolhadDousuario.equals("tesoura") && escolhaDaMaquina.equals("papel")))) {
             textResultado.setText("Você Ganhou");
         } else {
             textResultado.setText("Você Perdeu");

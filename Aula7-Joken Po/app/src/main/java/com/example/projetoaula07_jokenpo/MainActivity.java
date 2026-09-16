1225b5d5eed7a3c1b72734d965f7be4d5e6baa9a
Leonardo Cruz Lamari <25028194@fecap.br>
2026-09-15 21:09:53 -0300

    Finalização da 1° parte

--- Aula7-Joken Po/app/src/main/java/com/example/projetoaula07_jokenpo/MainActivity.java
+++ Aula7-Joken Po/app/src/main/java/com/example/projetoaula07_jokenpo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.projetoaula07_jokenpo;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtResultado;
+    ImageView imgMaquina,imgPedra,imgPapel,imgTesoura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,49 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtResultado = findViewById(R.id.resultado);
+
+        imgMaquina = findViewById(R.id.imageMaquina);
+        imgPedra = findViewById(R.id.imagePedra);
+        imgPapel = findViewById(R.id.imagePapel);
+        imgTesoura = findViewById(R.id.imageTesoura);
+
+        imgPedra.setOnClickListener(v -> jogar("pedra"));
+        imgPapel.setOnClickListener(v -> jogar("papel"));
+        imgTesoura.setOnClickListener(v -> jogar("tesoura"));
+    }
+
+    public void jogar(String escolhaPlayer)
+    {
+        String[] opcoes = {"pedra","papel","tesoura"};
+
+        int r = new Random().nextInt(3);
+        String escolhaMaquina = opcoes[r];
+
+        switch (escolhaMaquina)
+        {
+            case "pedra":
+                imgMaquina.setImageResource(R.drawable.pedra);
+                break;
+
+            case "papel":
+                imgMaquina.setImageResource(R.drawable.papel);
+                break;
+
+            case "tesoura":
+                imgMaquina.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+        if (escolhaPlayer.equals(escolhaMaquina)) {
+            txtResultado.setText("Empate");
+        } else if ((escolhaPlayer.equals("pedra") && escolhaMaquina.equals("tesoura")) ||
+                    (escolhaPlayer.equals("papel") && escolhaMaquina.equals("pedra")) ||
+                    (escolhaPlayer.equals("tesoura") && escolhaMaquina.equals("papel"))){
+            txtResultado.setText("Você ganhou");
+        } else {
+            txtResultado.setText("Você perdeu, otário");
+        }
     }
 }

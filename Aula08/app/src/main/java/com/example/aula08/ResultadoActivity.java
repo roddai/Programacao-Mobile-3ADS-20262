e1f7b1702d201b6cc6b1ff0dc9c1285865145b49
Katiely de Jesus Silva <23011355@fecap.br>
2026-09-22 22:18:06 -0300

    finaliação

--- Aula08/app/src/main/java/com/example/aula08/ResultadoActivity.java
+++ Aula08/app/src/main/java/com/example/aula08/ResultadoActivity.java
@@ -1,6 +1,8 @@
 package com.example.aula08;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +16,20 @@ public class ResultadoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_resultado);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView tvResultado = findViewById(R.id.tvResultado);
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+
+        String resultado = getIntent(). getStringExtra("resultado");
+        tvResultado.setText(resultado);
+
+        btnVoltar.setOnClickListener(v -> finish());
+
 
 }}

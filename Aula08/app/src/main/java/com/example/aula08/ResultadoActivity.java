64edf3f917441769fe7f5b06845a9bf17ec08cea
Álvaro Jordão de Carvalho Palazzin <25028302@fecap.br>
2026-09-22 22:21:40 -0300

    entrega final

--- Aula08/app/src/main/java/com/example/aula08/ResultadoActivity.java
+++ Aula08/app/src/main/java/com/example/aula08/ResultadoActivity.java
@@ -19,8 +19,8 @@ public class ResultadoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_resultado);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.resultado), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;

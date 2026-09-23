6a0b454c741f87a5abcead0f53ae79c060ae23e7
Maria Eduarda Barberino Olo <25028088@fecap.br>
2026-09-22 22:20:36 -0300

    Parte 2 Aaula8 Formulário

--- Aula082/app/src/main/java/com/example/aula08/MainActivity.java
+++ Aula082/app/src/main/java/com/example/aula08/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aula08;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button bntProximo = findViewById(R.id.bntProximo);
+        bntProximo.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, FormActivity.class);
+            startActivity(intent);
         });
     }
 }

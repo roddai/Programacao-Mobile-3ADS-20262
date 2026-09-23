cbfd56f649f5b6b1ad0bf308e361e3d81000400e
Matheus Marques Quio <23011107@fecap.br>
2026-09-22 22:34:33 -0300

    commit aula08

---
+++ Aula08/app/src/main/java/com/example/aula08/ResultadoActivity.java
@@ -0,0 +1,34 @@
+package com.example.aula08;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resultado);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView tvResultado = findViewById(R.id.tvResultado);
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+
+        String resultado = getIntent().getStringExtra("resultado");
+        tvResultado.setText(resultado);
+
+        btnVoltar.setOnClickListener(v -> finish());
+    }
+}
